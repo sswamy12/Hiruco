4311209f2d0dfd8270b4491b8ee2f61e69978dc5
sswamy12 <30510345+sswamy12@users.noreply.github.com>
2019-01-01 03:58:45 -0800

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Skanda Swamy on 1/1/19.
+//  Copyright © 2019 Hiruco.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
